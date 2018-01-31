f068fe3918ed9729368b54ffc8a3e84f0419e212
lucamota <36004082+lucamota@users.noreply.github.com>
2018-01-31 16:26:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 echo "Welcome to Rio de Janeiro!!<BR/>";
 
 
-<a style="font-face:'Verdana'; font-size:10; color:#FF6600";
-		  </a>
-
 ?>
 	
 <image src="http://ny2rio.com/wp-content/uploads/2012/09/rio-de-janeiro.jpg">  
