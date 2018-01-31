cb5eedc5e910d34068c9c96a1066e101a12d963f
lucamota <36004082+lucamota@users.noreply.github.com>
2018-01-31 16:10:31 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-echo "Hello word from Heroku!!<BR/>";
-echo "This file is stored on GitHub.<BR/>";
+echo "Welcome to Rio de Janeiro!!<BR/>";
+
 
 ?>
-<image src="https://students.dbs.ie/site_images/dbs_logo.png">
   
 <image src="http://ny2rio.com/wp-content/uploads/2012/09/rio-de-janeiro.jpg">  
