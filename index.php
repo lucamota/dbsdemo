30cd21a19a9b26295572d560bafa313a82e7f89c
lucamota <36004082+lucamota@users.noreply.github.com>
2018-01-31 16:22:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,10 @@
 
 echo "Welcome to Rio de Janeiro!!<BR/>";
 
-<table>
-  <tr>
-	<td bgcolor="<?php echo $bgcolor; ?>"> </td>
-  </tr>
-<table>
 
-	<a style="font-face:'Verdana'; font-size:1; color:#FF6600;
+<a style="font-face:'Verdana'; font-size:10; color:#FF6600;
 		  </a>
 
-
 ?>
-
 	
 <image src="http://ny2rio.com/wp-content/uploads/2012/09/rio-de-janeiro.jpg">  
