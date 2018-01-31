f8ff947bbc0549dd90ed43c30e197b22d960d839
lucamota <36004082+lucamota@users.noreply.github.com>
2018-01-31 16:08:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ echo "This file is stored on GitHub.<BR/>";
 ?>
 <image src="https://students.dbs.ie/site_images/dbs_logo.png">
   
-<image src="http://www.orangesmile.com/travelguide/rio-de-janeiro/photo-gallery.htm">  
+<image src="http://ny2rio.com/wp-content/uploads/2012/09/rio-de-janeiro.jpg">  
